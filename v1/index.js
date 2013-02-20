c991ea3de59b4a49260647bba39221a428622afd
Gareth Elms <garethmarkelms@gmail.com>
2013-02-20 20:27:04 +0000

    Fixed tab characters to be 2 spaces

--- v1/index.js
+++ v1/index.js
@@ -6,51 +6,51 @@ var fs = require("fs");
 var _formidable = require("formidable");
 
 var server = http.createServer(function(req, res) {
-	console.log(req.url);
-	switch(req.method) {
-		case "GET" : {
-			var url = req.url;
-			if(url == "/") url = "index.html";
-			serveFile(res, url);
-			break;
-		}
-		case "POST" : {
-			var form = new _formidable.IncomingForm();
-			form.parse(req, function(err, fields, files) {
-			  res.writeHead(200, {'content-type': 'text/plain'});
-			  res.write('received upload:\n\n');
-			  res.end(util.inspect({fields: fields, files: files}));
-			  //console.log(util.inspect({fields: fields, files: files}));
-			  serveFile(res, "index.html");
-			});
-			break;
-		}
-	};
-	
+  console.log(req.url);
+  switch(req.method) {
+    case "GET" : {
+      var url = req.url;
+      if(url == "/") url = "index.html";
+      serveFile(res, url);
+      break;
+    }
+    case "POST" : {
+      var form = new _formidable.IncomingForm();
+      form.parse(req, function(err, fields, files) {
+        res.writeHead(200, {'content-type': 'text/plain'});
+        res.write('received upload:\n\n');
+        res.end(util.inspect({fields: fields, files: files}));
+        //console.log(util.inspect({fields: fields, files: files}));
+        serveFile(res, "index.html");
+      });
+      break;
+    }
+  };
+  
 }).listen(3000);
 
 function serve404(res) {
-	res.writeHead(404, {"content-type": "text/plain"});
-	res.end("Error : Resource not found");
+  res.writeHead(404, {"content-type": "text/plain"});
+  res.end("Error : Resource not found");
 }
 
 function serveFile(res, fileName) {
-	fileName = "./public/" + fileName
-	fs.exists(fileName, function(exists) {
-		console.log("Does " + fileName + " exist?");
-		if(exists) {
-			fs.readFile(fileName, function(err, data) {
-				if(err) {
-					console.log("Nope.");
-					serve404(res);
-				} else {
-					console.log("Yes.");
-					res.writeHeader(200, {"content-type": mime.lookup(path.basename(fileName))});
-					res.end(data);
-				}
-			});
-		} else {
-			serve404(res);
-		}
-	});
+  fileName = "./public/" + fileName
+  fs.exists(fileName, function(exists) {
+    console.log("Does " + fileName + " exist?");
+    if(exists) {
+      fs.readFile(fileName, function(err, data) {
+        if(err) {
+          console.log("Nope.");
+          serve404(res);
+        } else {
+          console.log("Yes.");
+          res.writeHeader(200, {"content-type": mime.lookup(path.basename(fileName))});
+          res.end(data);
+        }
+      });
+    } else {
+      serve404(res);
+    }
+  });
 }
--- v2/index.js
+++ v2/index.js
@@ -7,67 +7,67 @@ var _formidable = require("formidable");
 var jade = require("jade");
 
 var server = http.createServer(function(req, res) {
-	console.log(req.url);
-	switch(req.method) {
-		case "GET" : {
-			var url = req.url;
-			//if(url == "/") url = "index.html";
-			//serveFile(res, url);
-			if(url == "/") {
-				url = "/index-test.jade";
-			}
+  console.log(req.url);
+  switch(req.method) {
+    case "GET" : {
+      var url = req.url;
+      //if(url == "/") url = "index.html";
+      //serveFile(res, url);
+      if(url == "/") {
+        url = "/index-test.jade";
+      }
       if(url.match(/^\/public\//)) {
-				serveFile(res, url);
-			} else {
-				serveJade(res, url);
-			}
-			break;
-		}
-		case "POST" : {
-			var form = new _formidable.IncomingForm();
-			form.parse(req, function(err, fields, files) {
-			  res.writeHead(200, {'content-type': 'text/plain'});
-			  res.write('received upload:\n\n');
-			  res.end(util.inspect({fields: fields, files: files}));
-			  //console.log(util.inspect({fields: fields, files: files}));
-			  serveFile(res, "index.html");
-			});
-			break;
-		}
-	};
-	
+        serveFile(res, url);
+      } else {
+        serveJade(res, url);
+      }
+      break;
+    }
+    case "POST" : {
+      var form = new _formidable.IncomingForm();
+      form.parse(req, function(err, fields, files) {
+        res.writeHead(200, {'content-type': 'text/plain'});
+        res.write('received upload:\n\n');
+        res.end(util.inspect({fields: fields, files: files}));
+        //console.log(util.inspect({fields: fields, files: files}));
+        serveFile(res, "index.html");
+      });
+      break;
+    }
+  };
+  
 }).listen(3000);
 
 function serve404(res) {
-	res.writeHead(404, {"content-type": "text/plain"});
-	res.end("Error : Resource not found");
+  res.writeHead(404, {"content-type": "text/plain"});
+  res.end("Error : Resource not found");
 }
 
 function serveJade(res, fileName) {
-	fileName = "./views" + fileName
-	fs.exists(fileName, function(exists) {
-		console.log("jade: Does " + fileName + " exist?");
-		if(exists) {
-			fs.readFile(fileName, function(err, data) {
-				if(err) {
-					console.log("Nope.");
-					serve404(res);
-				} else {
-					console.log("Yes....");
-					res.writeHeader(200, {"content-type": "text/html"}); //mime.lookup(path.basename(fileName))});
+  fileName = "./views" + fileName
+  fs.exists(fileName, function(exists) {
+    console.log("jade: Does " + fileName + " exist?");
+    if(exists) {
+      fs.readFile(fileName, function(err, data) {
+        if(err) {
+          console.log("Nope.");
+          serve404(res);
+        } else {
+          console.log("Yes....");
+          res.writeHeader(200, {"content-type": "text/html"}); //mime.lookup(path.basename(fileName))});
           //console.log("and.." + data);
-					var fn = jade.compile(data);
-					//console.log(util.inspect(fn));
-					var result = fn({});
-					//console.log(util.inspect(result));
-					//console.log(result);
-					res.end(result);
-				}
-			});
-		} else {
-			serve404(res);
-		}
-	});
+          var fn = jade.compile(data);
+          //console.log(util.inspect(fn));
+          var result = fn({});
+          //console.log(util.inspect(result));
+          //console.log(result);
+          res.end(result);
+        }
+      });
+    } else {
+      serve404(res);
+    }
+  });
 }
 
 function serveFile(res, fileName) {
@@ -76,21 +76,21 @@ function serveFile(res, fileName) {
   } else {
     fileName = "./public/" + fileName
   }
-	fs.exists(fileName, function(exists) {
-		console.log("Does " + fileName + " exist?");
-		if(exists) {
-			fs.readFile(fileName, function(err, data) {
-				if(err) {
-					console.log("Nope.");
-					serve404(res);
-				} else {
-					console.log("Yes.");
-					res.writeHeader(200, {"content-type": mime.lookup(path.basename(fileName))});
-					res.end(data);
-				}
-			});
-		} else {
-			serve404(res);
-		}
-	});
+  fs.exists(fileName, function(exists) {
+    console.log("Does " + fileName + " exist?");
+    if(exists) {
+      fs.readFile(fileName, function(err, data) {
+        if(err) {
+          console.log("Nope.");
+          serve404(res);
+        } else {
+          console.log("Yes.");
+          res.writeHeader(200, {"content-type": mime.lookup(path.basename(fileName))});
+          res.end(data);
+        }
+      });
+    } else {
+      serve404(res);
+    }
+  });
 }
