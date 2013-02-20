884c5a2a8f1e22577c3ca57e9b14f63596479dc1
Gareth Elms <garethmarkelms@gmail.com>
2013-02-20 20:04:01 +0000

    Initial commit

---
+++ v2/index.js
@@ -0,0 +1,96 @@
+var http = require("http");
+var util = require("util");
+var path = require("path");
+var mime = require("mime");
+var fs = require("fs");
+var _formidable = require("formidable");
+var jade = require("jade");
+
+var server = http.createServer(function(req, res) {
+	console.log(req.url);
+	switch(req.method) {
+		case "GET" : {
+			var url = req.url;
+			//if(url == "/") url = "index.html";
+			//serveFile(res, url);
+			if(url == "/") {
+				url = "/index-test.jade";
+			}
+      if(url.match(/^\/public\//)) {
+				serveFile(res, url);
+			} else {
+				serveJade(res, url);
+			}
+			break;
+		}
+		case "POST" : {
+			var form = new _formidable.IncomingForm();
+			form.parse(req, function(err, fields, files) {
+			  res.writeHead(200, {'content-type': 'text/plain'});
+			  res.write('received upload:\n\n');
+			  res.end(util.inspect({fields: fields, files: files}));
+			  //console.log(util.inspect({fields: fields, files: files}));
+			  serveFile(res, "index.html");
+			});
+			break;
+		}
+	};
+	
+}).listen(3000);
+
+function serve404(res) {
+	res.writeHead(404, {"content-type": "text/plain"});
+	res.end("Error : Resource not found");
+}
+
+function serveJade(res, fileName) {
+	fileName = "./views" + fileName
+	fs.exists(fileName, function(exists) {
+		console.log("jade: Does " + fileName + " exist?");
+		if(exists) {
+			fs.readFile(fileName, function(err, data) {
+				if(err) {
+					console.log("Nope.");
+					serve404(res);
+				} else {
+					console.log("Yes....");
+					res.writeHeader(200, {"content-type": "text/html"}); //mime.lookup(path.basename(fileName))});
+          //console.log("and.." + data);
+					var fn = jade.compile(data);
+					//console.log(util.inspect(fn));
+					var result = fn({});
+					//console.log(util.inspect(result));
+					//console.log(result);
+					res.end(result);
+				}
+			});
+		} else {
+			serve404(res);
+		}
+	});
+}
+
+function serveFile(res, fileName) {
+  if(fileName.match(/^\/public\//)) {
+    fileName = "." + fileName;
+  } else {
+    fileName = "./public/" + fileName
+  }
+	fs.exists(fileName, function(exists) {
+		console.log("Does " + fileName + " exist?");
+		if(exists) {
+			fs.readFile(fileName, function(err, data) {
+				if(err) {
+					console.log("Nope.");
+					serve404(res);
+				} else {
+					console.log("Yes.");
+					res.writeHeader(200, {"content-type": mime.lookup(path.basename(fileName))});
+					res.end(data);
+				}
+			});
+		} else {
+			serve404(res);
+		}
+	});
+}
